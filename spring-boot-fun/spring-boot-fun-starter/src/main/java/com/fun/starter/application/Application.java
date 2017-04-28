6a83b4dce8b70748fb1329b7605137af47cafa1a
lujun.xlj <lujun.xlj@alibaba-inc.com>
2017-04-28 17:57:57 +0800

    提交修改

--- spring-boot-fun/spring-boot-fun-starter/src/main/java/com/fun/starter/application/Application.java
+++ spring-boot-fun/spring-boot-fun-starter/src/main/java/com/fun/starter/application/Application.java
@@ -2,6 +2,8 @@ package com.fun.starter.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 
@@ -12,4 +14,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public FilterRegistrationBean registerFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        return bean;
+    }
+
 }
--- spring-boot-fun/spring-boot-fun-starter/src/main/java/com/fun/starter/web/MainController.java
+++ spring-boot-fun/spring-boot-fun-starter/src/main/java/com/fun/starter/web/MainController.java
@@ -1,4 +1,4 @@
-package web;
+package com.fun.starter.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
